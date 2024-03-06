c9faca037391f230f6976414e26dd4416ed16da9
Giacomo Ravetta <info@giacomoravetta.com>
2024-03-06 04:49:18 +0100

    Deleted old files

--- interactive-chart/src/components/UpdateButton.js
+++
@@ -1,9 +0,0 @@
-// UpdateButton.js
-import React from 'react';
-import '../css/UpdateButton.css';
-
-const UpdateButton = ({ onUpdate }) => {
-  return <button className="update-button" onClick={onUpdate}>Run animation</button>;
-};
-
-export default UpdateButton;
